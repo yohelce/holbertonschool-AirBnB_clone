449065fc671edb2839c8c9535ed7a6123bc9bf74
Your Name <yohel.cruz.espinoza@gmail.com>
2022-07-03 20:52:15 -0700

    Added __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module for FileStorage
+"""
+
+
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+
+classes = {"BaseModel": BaseModel,
+              "User": User,
+              "State": State,
+              "City": City,
+              "Place": Place,
+              "Amenity": Amenity,
+              "Review": Review}
+
+storage = FileStorage()
+storage.reload()
